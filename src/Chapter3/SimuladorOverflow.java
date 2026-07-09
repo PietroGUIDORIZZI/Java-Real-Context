48e9329eb324876e6dbcad70adb5c559f941ea0b
PietroBin <Peterguidorizzi@gmail.com>
2026-07-09 10:00:03 -0300

    Ex: 3.2.1

--- src/Chapter3/SimuladorOverflow.java
+++ src/Chapter3/SimuladorOverflow.java
@@ -4,9 +4,14 @@ public class SimuladorOverflow {
     static void main(String[] args) {
         int contador = Integer.MAX_VALUE;
         System.out.println("Valor máximo de int: " + contador);
+        try {
+        Math.addExact(contador, 1);
 
-        contador++;
-        System.out.println("Após somar 1: " + contador + " (overflow!)");
+        }catch (ArithmeticException ae){
+            System.out.println("Overflow Capturado!");
+            contador++;
+            System.out.println("Após somar 1: " + contador +  " (overflow!)");
+        }
         contador--;
         long contadorL = contador;
         System.out.println("Valor de long: " + contadorL);
