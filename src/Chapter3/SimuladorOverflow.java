8c6b4e21e55fd84f219660d233cf9689c5ab8b0a
PietroBin <Peterguidorizzi@gmail.com>
2026-07-09 09:55:55 -0300

    Ex: 3.2

---
+++ src/Chapter3/SimuladorOverflow.java
@@ -0,0 +1,17 @@
+package Chapter3;
+
+public class SimuladorOverflow {
+    static void main(String[] args) {
+        int contador = Integer.MAX_VALUE;
+        System.out.println("Valor máximo de int: " + contador);
+
+        contador++;
+        System.out.println("Após somar 1: " + contador + " (overflow!)");
+        contador--;
+        long contadorL = contador;
+        System.out.println("Valor de long: " + contadorL);
+
+        contadorL++;
+        System.out.println("Soma-se 1 sem overflow: " + contadorL);
+    }
+}
