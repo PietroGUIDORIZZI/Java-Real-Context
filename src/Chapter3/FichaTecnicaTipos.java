04e8f809279ca7e98a24cdf18f457c256da1ecc0
PietroBin <Peterguidorizzi@gmail.com>
2026-07-09 09:49:50 -0300

    Ex: 3.1

---
+++ src/Chapter3/FichaTecnicaTipos.java
@@ -0,0 +1,17 @@
+package Chapter3;
+
+import java.util.Formatter;
+
+public class FichaTecnicaTipos {
+    public static void main(String[] args){
+        System.out.printf("Byte Range: ");
+        System.out.println(Byte.MIN_VALUE + " " + Byte.MAX_VALUE);
+        System.out.printf("Short Range: ");
+        System.out.println(Short.MIN_VALUE + " " + Short.MAX_VALUE);
+        System.out.printf("Integer Range: ");
+        System.out.println(Integer.MIN_VALUE + " " + Integer.MAX_VALUE);
+        System.out.printf("Long Range: ");
+        System.out.println(Long.MIN_VALUE + " " + Long.MAX_VALUE);
+
+    }
+}
